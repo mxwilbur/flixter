6c863600c53567fa7ba36f576357f0d7774f5728
mxwilbur <mxwilbur@outlook.com>
2019-07-01 01:33:33 +0000

    updated booyah-box

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,8 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  # Recoverable module selected so changing from ':debug' to ':warn'
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
