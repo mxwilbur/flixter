bcd3189c2e32c2b8448f18e1689f5a44979a409e
mxwilbur <mxwilbur@outlook.com>
2019-07-08 18:48:35 +0000

    show lesson detail

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,11 @@
 class LessonsController < ApplicationController
   def show
   end
+
+  private
+
+  helper_method :current_lesson
+  def current_lesson
+    @cureent_lesson ||= Lesson.find(params[:id])
+  end
 end
