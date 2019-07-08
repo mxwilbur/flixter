014b0d23584778086cdcb6b3d21eaa38b7423907
mxwilbur <mxwilbur@outlook.com>
2019-07-08 18:53:54 +0000

    lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,6 +6,6 @@ class LessonsController < ApplicationController
 
   helper_method :current_lesson
   def current_lesson
-    @cureent_lesson ||= Lesson.find(params[:id])
+    @current_lesson ||= Lesson.find(params[:id])
   end
 end
