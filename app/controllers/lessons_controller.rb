2d26c6a3a297560c0146e8927b5758c11eed3e47
mxwilbur <mxwilbur@outlook.com>
2019-07-13 15:12:34 +0000

    editing view lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,16 +1,21 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:show]
+  before_action :require_enrolled_for_current_course, only: [:show]
 
   def show
   end
 
   private
 
-  def require_authorized_for_current_course
+  def require_enrolled_for_current_course
     if current_course.user != current_user
       redirect_to course_path(current_course), alert: 'Please enroll in course to view lesson'
     end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
   end
 
   helper_method :current_lesson
