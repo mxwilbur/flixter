52718ff97cdfe6ad8fbffe6d018906b8374020eb
mxwilbur <mxwilbur@outlook.com>
2019-07-13 14:53:40 +0000

    add permission to view lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
 
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      redirect_to course_path(current_course), alert: 'Please enroll in course to view lesson'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
