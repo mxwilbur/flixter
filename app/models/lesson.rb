6ebf8156f861258bea6d7078cc001c0ada050c45
mxwilbur <mxwilbur@outlook.com>
2019-07-15 22:59:54 +0000

    load RankedModel

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,7 +2,7 @@ class Lesson < ApplicationRecord
   mount_uploader :video, VideoUploader
   belongs_to :section
 
-  include 
+  include RankedModel
   ranks :row_order, with_same: :section_id
   
 end
